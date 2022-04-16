4ed047a28c4fa4484d0a352d496bdf9949fb3953
javeriabaqir <javeriabaqir75@gmail.com>
2022-04-16 09:23:53 -0700

    work complete

---
+++ app.js
@@ -0,0 +1,98 @@
+// Chapter 1/
+// Answer # 1
+
+//  alert("hello world");
+
+// Answer # 2
+//  alert("Error! please enter a valid password.");
+
+//Answer # 3
+
+//  alert("Welcome to JS land...\n happy coding!");
+
+//Answer # 4
+
+// alert("welcome to JS land");
+// alert("happy coding! \n prevent this page from creating addtioinal dialogs!");
+ 
+
+// Answer # 5
+// alert("hello... I can run JS through my web browser console!");
+
+
+// Chapter 2
+// Answer # 4
+
+// var a = "johne doe";
+// alert(a);
+
+// var b = "15 years old";
+// alert(b);
+
+// var c = "certified mobile application development";
+// alert(c);
+
+
+// chapter 4
+// Answer # 1
+
+// var name,age,nationality;
+
+// Answer # 2
+
+// var name,age,ntionality,product,store(legal names);
+// var 1stperson,alert,var,2sid,"name"(lllegal names);
+
+// Answer # 3
+
+// var a = "Rules for naming JS variables";
+// var b = "\nvariable names can only contain numbers,$ and _. for example variable";
+// var c = "variable mmust begin with a letter, $ or _. For example $name,_name or name";
+// var d = "variable names are case sensitive";
+// var e = "Variable names shouldnot be JS keywords";
+
+// document.write(a+"<br>" b+"<br>");
+// var f = (a+b+c+d+e)
+
+// var a = "Rules for making JS variables rules for naming JS variable naming JS variables \nRules for making JS variables";
+// document.write(a);
+
+// Chapter 5
+// Answer # 1
+
+// var a = "3";
+// var b = "5";
+// var c = (a+b);
+// alert(c);
+
+// Answer # 2
+
+// var a = "3";
+// var b = "5";
+// var c = (a-b);
+// alert(c);
+
+
+
+// var a = "3";
+// var b = "5";
+// var c = (a*b);
+// alert(c);
+
+
+// var a = "3";
+// var b = "5";
+// var c = (a/b);
+// alert(c);
+
+
+
+
+
+
+
+
+
+
+
+
