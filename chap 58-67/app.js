8b255e0a0476923d604d2bbdce3f4ce7922773bd
javeriabaqir <javeriabaqir75@gmail.com>
2022-07-16 07:52:45 -0700

    done

---
+++ chap 58-67/app.js
@@ -0,0 +1,99 @@
+// chap 58 to 67
+// (I)
+
+// var a = document.getElementById("main-content")
+// console.log(a)
+
+// (II)
+
+// var b = document.getElementById("main-content").childNodes
+// console.log(b)
+
+// (III)
+
+// var c = document.getElementsByClassName("render")
+// document.write(c[0])
+// document.write(c[1])
+// document.write(c[2])
+// document.write(c[3])
+// document.write(c[4])
+// document.write(c[5])
+
+
+
+// (IV)
+// var a = document.getElementById("first-name")
+// var  text=document.createTextNode("zeeshan saleem")
+// a.appendChild(text)
+// console.log(a)
+
+
+// // (V)
+// var b = document.getElementById("last-name")
+// var  text=document.createTextNode("saleem")
+// b.appendChild(text)
+// console.log(b)
+
+// // (VI)
+// var c = document.getElementById("email")
+// var  text=document.createTextNode("abc@gmail.com")
+// c.appendChild(text)
+// console.log(c)
+
+
+
+// question2
+// (I)
+
+// var a = document.getElementById("form-content").nodeType
+// console.log(a)
+
+// (II)
+
+// var b = document.getElementById("lastName").nodeType
+// console.log(b)
+
+// var c = document.getElementById("lastName").childNodes
+// console.log(c.nodeType)
+
+
+// (III)
+
+// var d = document.getElementById("lastName")
+// var text = document.createTextNode(" of pakistan")
+// d.appendChild(text)
+
+// (IV)
+
+// var e = document.getElementById("main-content").firstChild
+// console.log(e)
+
+// var f = document.getElementById("main-content").lastChild
+// console.log(f)
+
+
+// (V)
+
+// var g = document.getElementById("lastName").previousSibling
+// console.log(g)
+
+// var h = document.getElementById("lastName").nextSibling
+// console.log(h)
+
+
+
+// (VI)
+
+// var i = document.getElementById("email").parentNode
+// console.log(i)
+
+// var j = document.getElementById("email")
+// console.log(j)
+
+
+
+
+
+
+
+    
---
+++ chp 49-52/app.js
@@ -0,0 +1,63 @@
+// chap 49- 52
+    // task1
+
+    // function sub(){
+    //     var a = document.getElementById("name")
+    //     console.log ("name :" + a.value)
+    //     var b = document.getElementById("email")
+    //     console.log("email :" + b.value)
+    //     var c = document.getElementById("password")
+    //     console.log("password :" + c.value)
+    // }
+
+    // task 2
+    // function readmore(){
+    //     var text = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex accusamus itaque deserunt maiores perspiciatis necessitatibus animi molestiae laudantium, magni minima minus laboriosam nam eligendi distinctio accusantium! Aliquam, dicta consectetur? Exercitationem?"
+    //     var para = document.getElementById("para")
+    //     para.innerHTML = text
+    // }
+
+
+
+    // task 3
+    // var row = 1;
+    // var abc = document.getElementById("abc");
+    // abc.addEventListener("click" ,maketable)
+    // // abc.addEventListener('click',display)
+  
+    // function maketable(){
+    //     var name = document.getElementById("name").value
+    //     // console.log(name)
+    //     var rollnum =document.getElementById("rollnum").value
+    //     // console.log(rollnum)
+    //     var grade = document.getElementById("grade").value
+    //     // console.log(grade)
+
+    //     if (!name || !grade ||!rollnum){
+    //         alert("fill all boxes")
+    //         return ;
+
+    //     }
+    //     var display = document.getElementById("display")
+    //     var newRow = display.insertRow(row);
+
+
+    //     var cell1 = newRow.insertCell(0);
+    //     var cell2 = newRow.insertCell(1);
+    //     var cell3 = newRow.insertCell(2);
+    //     var cell4 = newRow.insertCell(3)
+
+    //     cell1.innerHtml =name;
+    //     cell2.innerHtml=rollnum;
+    //     cell3.innerHtml=grade;
+    //     cell4.innerHtml = button
+        
+
+    //     row++;
+
+
+    // }
+
+
+
+
